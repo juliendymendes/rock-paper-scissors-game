6ffbc392d092f85cda9a56b99707e1a78ee1a9d3
Juliendy Mendes <juliendy0708@gmail.com>
2023-05-14 17:26:17 -0400

    feat: rules button

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import RulesButton from './components/RulesButton.vue';
 </script>
 
 <template>
   <RouterView />
+  <RulesButton/>
 </template>
 
 <style scoped>
