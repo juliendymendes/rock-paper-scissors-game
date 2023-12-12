d1f6bbbfad8667164b070eb6c7a19a2b6967d9c9
Juliendy Mendes <juliendy0708@gmail.com>
2023-12-12 10:23:01 -0400

    feat: rules modal component

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import { RouterView } from 'vue-router'
 import RulesButton from './components/RulesButton.vue';
 import HeaderComponent from './components/HeaderComponent.vue';
+import RulesModalComponent from './components/RulesModalComponent.vue';
 </script>
 
 <template>
   <HeaderComponent/>
+	<!-- <RulesModalComponent/> -->
   <RouterView />
   <RulesButton/>
 </template>
---
+++ src/components/RulesModalComponent.vue
@@ -0,0 +1,57 @@
+<template>
+  <dialog>
+    <div class="modal-header">
+      <button>Fechar</button>
+      <p>Rules</p>
+    </div>
+    <img src="/images/image-rules.svg" alt="Rules" width="280" />
+  </dialog>
+</template>
+
+<script setup></script>
+
+<style lang="scss" scoped>
+dialog {
+  background-color: white;
+  border-radius: 12px;
+  border: none;
+  width: 400px;
+  height: 400px;
+  margin: auto;
+  z-index: 3;
+
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+
+  .modal-header {
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    flex-direction: row-reverse;
+    width: 100%;
+    margin-bottom: 38px;
+
+    p {
+      align-self: flex-start;
+      margin-left: 32px;
+      font-size: 28px;
+      text-transform: uppercase;
+      color: var(--dark);
+    }
+
+    button {
+      border: none;
+      background-color: transparent;
+      background-image: url("/images/icon-close.svg");
+      background-repeat: no-repeat;
+      color: transparent;
+      width: 20px;
+      height: 20px;
+      align-self: flex-end;
+      margin-right: 32px;
+    }
+  }
+}
+</style>
